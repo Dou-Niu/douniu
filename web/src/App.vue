cbc50f431da226adfbb6f52dc9594418255fee33
unknown <2524447983@qq.com>
2023-10-28 13:18:39 +0800

    添加elementplus组件库

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-    <el-icon :size="20">
-    <Edit />
-  </el-icon>
+
   </div>
 </template>
 
@@ -16,6 +8,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

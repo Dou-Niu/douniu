0d85fe07aad62cf365fb272bbb1e2e44642b4b38
liuxian <2929504046@qq.com>
2023-10-29 20:16:25 +0800

    video rpc添加一些

--- server/video/rpc/internal/logic/get_video_list_info_logic.go
+++ server/video/rpc/internal/logic/get_video_list_info_logic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"douniu/server/common/errorx"
+	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/logc"
 
 	"douniu/server/video/rpc/internal/svc"
 	"douniu/server/video/rpc/types/pb"
@@ -24,7 +27,29 @@ func NewGetVideoListInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *GetVideoListInfoLogic) GetVideoListInfo(in *pb.GetVideoListInfoReq) (*pb.GetVideoListInfoResp, error) {
-	// todo: add your logic here and delete this line
+	// 取出视频信息
+	res := make([]*pb.Video, 0)
+	for _, v := range in.VideoIdList {
+		oneVideo, err := l.svcCtx.VideoModel.FindOne(l.ctx, v)
+		if err != nil {
+			logc.Error(l.ctx, err)
+			return nil, errors.Wrapf(errorx.NewDefaultError("mysql查询视频失败"+err.Error()), "更改用户密码失败ResetPassword：%v", in)
+			// TODO 调用其他rpc
+		}
+		res = append(res, &pb.Video{
+			Id:            oneVideo.Id,
+			User:          nil,
+			PlayUrl:       oneVideo.PlayUrl,
+			CoverUrl:      oneVideo.CoverUrl,
+			FavoriteCount: 0,
+			CommentCount:  0,
+			IsFavorite:    false,
+			Title:         oneVideo.Title,
+			Partition:     oneVideo.Partition,
+		})
+	}
 
-	return &pb.GetVideoListInfoResp{}, nil
+	return &pb.GetVideoListInfoResp{
+		VideoList: res,
+	}, nil
 }

784c2b81de0e2d434cdbcfe90cccd88e9a49fb0e
liuxian <2929504046@qq.com>
2023-10-30 16:51:33 +0800

    完成热门视频

--- server/video/rpc/internal/logic/feed_hot_logic.go
+++ server/video/rpc/internal/logic/feed_hot_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"douniu/server/common/consts"
 
 	"douniu/server/video/rpc/internal/svc"
 	"douniu/server/video/rpc/types/pb"
@@ -13,18 +14,34 @@ type FeedHotLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 	logx.Logger
+	GetVideoListInfo *GetVideoListInfoLogic
 }
 
 func NewFeedHotLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FeedHotLogic {
 	return &FeedHotLogic{
-		ctx:    ctx,
-		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
+		ctx:              ctx,
+		svcCtx:           svcCtx,
+		Logger:           logx.WithContext(ctx),
+		GetVideoListInfo: NewGetVideoListInfoLogic(ctx, svcCtx),
 	}
 }
 
 func (l *FeedHotLogic) FeedHot(in *pb.FeedHotReq) (*pb.FeedHotResp, error) {
-	// todo: add your logic here and delete this line
+	videoIds, err := l.svcCtx.VideoModel.FindByTimeOrHot(l.ctx, l.svcCtx.RedisClient, consts.VideoHotScore, in.MaxHot)
+	if err != nil {
+		return nil, err
+	}
+	videoList, err := l.GetVideoListInfo.GetVideoListInfo(&pb.GetVideoListInfoReq{
+		MeUserId:    in.MeUserId,
+		VideoIdList: videoIds,
+	})
+	if err != nil {
+		return nil, err
+	}
+	list := videoList.VideoList
 
-	return &pb.FeedHotResp{}, nil
+	return &pb.FeedHotResp{
+		NextMaxHost: list[len(list)-1].CreateTime,
+		VideoList:   list,
+	}, nil
 }
